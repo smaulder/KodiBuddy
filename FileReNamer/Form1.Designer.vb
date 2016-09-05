303ad49b1795c22f2d904ecf47e65ce744dd7db0
Steve <stevemauldin@hotmail.com>
2016-09-05 00:42:32 -0500

    working version 1.0

--- FileReNamer/Form1.Designer.vb
+++ FileReNamer/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,153 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.BtnSelectFolder = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.txtFolderPath = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CkBxUseBracket = New System.Windows.Forms.CheckBox()
+        Me.CkBxUseParens = New System.Windows.Forms.CheckBox()
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.LblMessage = New System.Windows.Forms.Label()
+        Me.BtnIMDB = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LblRecordCount = New System.Windows.Forms.Label()
+        Me.BtnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnSelectFolder
+        '
+        Me.BtnSelectFolder.Location = New System.Drawing.Point(612, 21)
+        Me.BtnSelectFolder.Name = "BtnSelectFolder"
+        Me.BtnSelectFolder.Size = New System.Drawing.Size(21, 23)
+        Me.BtnSelectFolder.TabIndex = 0
+        Me.BtnSelectFolder.Text = "..."
+        Me.BtnSelectFolder.UseVisualStyleBackColor = True
+        '
+        'txtFolderPath
+        '
+        Me.txtFolderPath.Location = New System.Drawing.Point(295, 21)
+        Me.txtFolderPath.Name = "txtFolderPath"
+        Me.txtFolderPath.Size = New System.Drawing.Size(314, 20)
+        Me.txtFolderPath.TabIndex = 1
+        Me.txtFolderPath.Text = "V:\"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(222, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Folder Path:"
+        '
+        'CkBxUseBracket
+        '
+        Me.CkBxUseBracket.AutoSize = True
+        Me.CkBxUseBracket.Location = New System.Drawing.Point(298, 87)
+        Me.CkBxUseBracket.Name = "CkBxUseBracket"
+        Me.CkBxUseBracket.Size = New System.Drawing.Size(77, 17)
+        Me.CkBxUseBracket.TabIndex = 7
+        Me.CkBxUseBracket.Text = "Use [Date]"
+        Me.CkBxUseBracket.UseVisualStyleBackColor = True
+        '
+        'CkBxUseParens
+        '
+        Me.CkBxUseParens.AutoSize = True
+        Me.CkBxUseParens.Location = New System.Drawing.Point(384, 87)
+        Me.CkBxUseParens.Name = "CkBxUseParens"
+        Me.CkBxUseParens.Size = New System.Drawing.Size(77, 17)
+        Me.CkBxUseParens.TabIndex = 8
+        Me.CkBxUseParens.Text = "Use (Date)"
+        Me.CkBxUseParens.UseVisualStyleBackColor = True
+        '
+        'BtnStart
+        '
+        Me.BtnStart.Location = New System.Drawing.Point(295, 123)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(123, 23)
+        Me.BtnStart.TabIndex = 9
+        Me.BtnStart.Text = "Change Date Brackets"
+        Me.BtnStart.UseVisualStyleBackColor = True
+        '
+        'LblMessage
+        '
+        Me.LblMessage.AutoSize = True
+        Me.LblMessage.Location = New System.Drawing.Point(222, 160)
+        Me.LblMessage.Name = "LblMessage"
+        Me.LblMessage.Size = New System.Drawing.Size(0, 13)
+        Me.LblMessage.TabIndex = 10
+        '
+        'BtnIMDB
+        '
+        Me.BtnIMDB.Location = New System.Drawing.Point(434, 123)
+        Me.BtnIMDB.Name = "BtnIMDB"
+        Me.BtnIMDB.Size = New System.Drawing.Size(84, 23)
+        Me.BtnIMDB.TabIndex = 11
+        Me.BtnIMDB.Text = "IMDB Update"
+        Me.BtnIMDB.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(292, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Folder Count:"
+        '
+        'LblRecordCount
+        '
+        Me.LblRecordCount.AutoSize = True
+        Me.LblRecordCount.Location = New System.Drawing.Point(368, 56)
+        Me.LblRecordCount.Name = "LblRecordCount"
+        Me.LblRecordCount.Size = New System.Drawing.Size(0, 13)
+        Me.LblRecordCount.TabIndex = 13
+        '
+        'BtnCancel
+        '
+        Me.BtnCancel.Location = New System.Drawing.Point(534, 123)
+        Me.BtnCancel.Name = "BtnCancel"
+        Me.BtnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.BtnCancel.TabIndex = 14
+        Me.BtnCancel.Text = "Cancel"
+        Me.BtnCancel.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(910, 209)
+        Me.Controls.Add(Me.BtnCancel)
+        Me.Controls.Add(Me.LblRecordCount)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BtnIMDB)
+        Me.Controls.Add(Me.LblMessage)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Controls.Add(Me.CkBxUseParens)
+        Me.Controls.Add(Me.CkBxUseBracket)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtFolderPath)
+        Me.Controls.Add(Me.BtnSelectFolder)
+        Me.Name = "Form1"
+        Me.Text = "Movie Folder Cleaner"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents BtnSelectFolder As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents txtFolderPath As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CkBxUseBracket As CheckBox
+    Friend WithEvents CkBxUseParens As CheckBox
+    Friend WithEvents BtnStart As Button
+    Friend WithEvents LblMessage As Label
+    Friend WithEvents BtnIMDB As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LblRecordCount As Label
+    Friend WithEvents BtnCancel As Button
 End Class
--- FileReNamer/Form1.vb
+++ FileReNamer/Form1.vb
@@ -1,3 +1,215 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+
+Public Class Form1
+    Public Dirs() As String
+
+    Private Sub BtnSelectFolder_Click(sender As Object, e As EventArgs) Handles BtnSelectFolder.Click
+        FolderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer
+        FolderBrowserDialog1.ShowDialog()
+        txtFolderPath.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+
+    Private Sub CkBxUseBracket_CheckedChanged(sender As Object, e As EventArgs) Handles CkBxUseBracket.CheckedChanged
+        If CkBxUseParens.Checked And CkBxUseBracket.Checked Then
+            CkBxUseParens.Checked = False
+        End If
+    End Sub
+
+    Private Sub CkBxUseParens_CheckedChanged(sender As Object, e As EventArgs) Handles CkBxUseParens.CheckedChanged
+        If CkBxUseParens.Checked And CkBxUseBracket.Checked Then
+            CkBxUseBracket.Checked = False
+        End If
+    End Sub
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        Try
+            BtnStart.Text = "Processing"
+            BtnStart.Enabled = False
+            Me.Refresh()
+
+            Dim DirList As New ArrayList
+            Dim ResultName As String = ""
+
+            LblMessage.Text = "Starting processing"
+            LblMessage.ForeColor = Color.Black
+            Me.Refresh()
+
+            For i As Integer = Dirs.Count - 1 To 0 Step -1
+                Dim Dir As String = Dirs(i)
+                If CkBxUseParens.Checked = True And (Dir.Remove(0, Dir.LastIndexOf("\") + 1).LastIndexOf("[") > 0 Or Dir.Remove(0, Dir.LastIndexOf("\") + 1).LastIndexOf("]") > 0) Then
+                    ResultName = Dir.Remove(0, Dir.LastIndexOf("\") + 1).Replace("[", "(").Replace("]", ")")
+                    LblMessage.Text = Dir
+                    LblMessage.ForeColor = Color.Black
+                    My.Computer.FileSystem.RenameDirectory(Dir, ResultName)
+                    Me.Refresh()
+                ElseIf CkBxUseBracket.Checked = True And (Dir.Remove(0, Dir.LastIndexOf("\") + 1).LastIndexOf("(") > 0 Or Dir.Remove(0, Dir.LastIndexOf("\") + 1).LastIndexOf(")") > 0) Then
+                    ResultName = Dir.Remove(0, Dir.LastIndexOf("\") + 1).Replace("(", "[").Replace(")", "]")
+                    LblMessage.Text = Dir
+                    LblMessage.ForeColor = Color.Black
+                    My.Computer.FileSystem.RenameDirectory(Dir, ResultName)
+                    Me.Refresh()
+                    'ElseIf (Dir.Remove(0, Dir.LastIndexOf("\") + 1).LastIndexOf("[") = 0 And Dir.Remove(0, Dir.LastIndexOf("\") + 1).LastIndexOf("]") = 0) Then
+                    '    'ToDo: lookup directory on imdb.
+                    '    ResultName = Dir.Remove(0, Dir.LastIndexOf("\") + 1)
+                    '    LblMessage.Text = Dir
+                    '    LblMessage.ForeColor = Color.OrangeRed
+                    '    Me.Refresh()
+                End If
+            Next i
+
+            BtnStart.Text = "Start"
+            BtnStart.Enabled = True
+
+            LblMessage.Text = "Processing done on " & Dirs.Count & " Directories."
+            LblMessage.ForeColor = Color.Black
+            Me.Refresh()
+
+            'Reload Folders
+            Dirs = getAllFolders(txtFolderPath.Text)
+
+        Catch ex As Exception
+            LblMessage.Text = ex.Message
+            LblMessage.ForeColor = Color.OrangeRed
+        End Try
+
+
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtFolderPath.Text = "V:\"
+        Dirs = getAllFolders(txtFolderPath.Text)
+        CkBxUseParens.Checked = True
+        LblRecordCount.Text = Dirs.Count
+    End Sub
+
+
+    Private Sub BtnIMDB_Click(sender As Object, e As EventArgs) Handles BtnIMDB.Click
+        Try
+            BtnStart.Text = "Processing"
+            BtnStart.Enabled = False
+            Me.Refresh()
+
+            Dim DirList As New ArrayList
+            Dim ResultName As String = ""
+            Dim Resultant As String = ""
+
+            LblMessage.Text = "Starting processing"
+            LblMessage.ForeColor = Color.Black
+            Me.Refresh()
+
+            For i As Integer = Dirs.Count - 1 To 0 Step -1
+                Dim Dir As String = Dirs(i)
+                If (Dir.Remove(0, Dir.LastIndexOf("\") + 1).LastIndexOf("[") > 0 Or Dir.Remove(0, Dir.LastIndexOf("\") + 1).LastIndexOf("]") > 0) Or (Dir.Remove(0, Dir.LastIndexOf("\") + 1).LastIndexOf("(") > 0 Or Dir.Remove(0, Dir.LastIndexOf("\") + 1).LastIndexOf(")") > 0) Then
+                    'date is already on the folder
+                Else
+
+                    Resultant = Dir.Remove(0, Dir.LastIndexOf("\") + 1)
+                    ResultName = Dir.Remove(0, Dir.LastIndexOf("\") + 1)
+                    If ResultName.ToUpper() <> UCase("Western") And
+                        ResultName.ToUpper() <> UCase("War") And
+                        ResultName.ToUpper() <> UCase("Video") And
+                        ResultName.ToUpper() <> UCase("Thriller") And
+                        ResultName.ToUpper() <> UCase("Super Hero") And
+                        ResultName.ToUpper() <> UCase("Sports") And
+                        ResultName.ToUpper() <> UCase("SciFi-Fantasy") And
+                        ResultName.ToUpper() <> UCase("Romance") And
+                        ResultName.ToUpper() <> UCase("Mystery") And
+                        ResultName.ToUpper() <> UCase("Musical") And
+                        ResultName.ToUpper() <> UCase("Horror-Thriller") And
+                        ResultName.ToUpper() <> UCase("Holiday") And
+                        ResultName.ToUpper() <> UCase("Historical Drama") And
+                        ResultName.ToUpper() <> UCase("Foreign") And
+                        ResultName.ToUpper() <> UCase("Drama") And
+                        ResultName.ToUpper() <> UCase("Documentary") And
+                        ResultName.ToUpper() <> UCase("Cult Classics") And
+                        ResultName.ToUpper() <> UCase("Crime Dramas") And
+                        ResultName.ToUpper() <> UCase("Concerts") And
+                        ResultName.ToUpper() <> UCase("Comedy") And
+                        ResultName.ToUpper() <> UCase("Childrens") And
+                        ResultName.ToUpper() <> UCase("Biography") And
+                        ResultName.ToUpper() <> UCase("Apocalyptic") And
+                        ResultName.ToUpper() <> UCase("Action-Adventure") Then
+
+                        LblMessage.Text = Dir
+                        LblMessage.ForeColor = Color.OrangeRed
+                        Me.Refresh()
+
+                        'ToDo: lookup directory on imdb.
+                        Dim webClient As New System.Net.WebClient
+                        Dim result As String = webClient.DownloadString("https://www.themoviedb.org/search?query=" & Resultant.Replace(" ", "+"))
+                        Dim dateA As String = Mid(result, result.IndexOf("<span class=""release_date"">") + 29, 10)
+                        If IsDate(dateA) Then
+                            Dim dateyear As String = Year(dateA)
+                            If CkBxUseParens.Checked = True Then
+                                ResultName = ResultName & " (" & dateyear & ")"
+                            Else
+                                ResultName = ResultName & " [" & dateyear & "]"
+                            End If
+                            Try
+                                My.Computer.FileSystem.RenameDirectory(Dir, ResultName)
+                            Catch ex As Exception
+                                Dim a = ex.Message
+                            End Try
+                        End If
+                    End If
+                End If
+            Next i
+
+            BtnStart.Text = "Start"
+            BtnStart.Enabled = True
+
+            LblMessage.Text = "Processing done on " & Dirs.Count & " Directories."
+            LblMessage.ForeColor = Color.Black
+            Me.Refresh()
+
+            'Reload Folders
+            Dirs = getAllFolders(txtFolderPath.Text)
+
+        Catch ex As Exception
+            LblMessage.Text = ex.Message
+            LblMessage.ForeColor = Color.OrangeRed
+        End Try
+    End Sub
+
+
+    Private Function getAllFolders(ByVal directory As String) As String()
+        'Create object
+        Dim fi As New IO.DirectoryInfo(directory)
+        'Array to store paths
+        Dim path() As String = {}
+        Dim ellipsis As String = ""
+        Try
+            'Loop through subfolders
+            For Each subfolder As IO.DirectoryInfo In fi.GetDirectories()
+
+                If UCase(subfolder.FullName.Remove(0, subfolder.FullName.LastIndexOf("\") + 1)) <> UCase("$recycle.bin") And
+                    UCase(subfolder.FullName.Remove(0, subfolder.FullName.LastIndexOf("\") + 1)) <> UCase("extrafanart") And
+                    UCase(subfolder.FullName.Remove(0, subfolder.FullName.LastIndexOf("\") + 1)) <> UCase("extrathumbs") And
+                    UCase(subfolder.FullName.Remove(0, subfolder.FullName.LastIndexOf("\") + 1)) <> UCase("subs") And
+                    UCase(subfolder.FullName.Remove(0, subfolder.FullName.LastIndexOf("\") + 1)) <> UCase("subtitles") Then
+
+                    'Add this folders name
+                    Array.Resize(path, path.Length + 1)
+                    path(path.Length - 1) = subfolder.FullName
+                    'Recall function with each subdirectory
+
+                    For Each s As String In getAllFolders(subfolder.FullName)
+                        Array.Resize(path, path.Length + 1)
+                        path(path.Length - 1) = s
+                    Next
+                End If
+            Next
+        Catch ex As Exception
+            LblMessage.Text = ex.Message
+            LblMessage.ForeColor = Color.OrangeRed
+        End Try
+
+        Return path
+    End Function
+
+    Private Sub BtnCancel_Click(sender As Object, e As EventArgs) Handles BtnCancel.Click
+        End
+    End Sub
 End Class
