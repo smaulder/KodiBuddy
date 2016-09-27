88adb29518dfb3a7055e2cff597b57a99e29a3dd
Steve <stevemauldin@hotmail.com>
2016-09-26 21:12:02 -0500

    renamed to KodiBuddy

--- KodiBuddy/My Project/Application.Designer.vb
+++ KodiBuddy/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.KodiBuddy.Form1
+            Me.MainForm = Global.KodiBuddy.FrmKodiBuddy
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
